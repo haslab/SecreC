408dd6e2c030cf6f2af3a9b45200f0a63a500021
Hugo Pacheco <hpacheco@gmail.com>
2016-11-13 00:04:56 +0000

    blah

--- src/Language/SecreC/Transformation/Dafny.hs
+++ src/Language/SecreC/Transformation/Dafny.hs
@@ -269,7 +269,7 @@ resolveEntryPoint n = do
                     Nothing -> return Nothing
             otherwise -> return Nothing
         otherwise -> return Nothing
-    debugTc $ liftIO $ putStrLn $ "resolveEntryPoint " ++ pprid n ++ " " ++ pprid mb ++ " in\n" ++ show (procedures env)
+    debugTc $ liftIO $ putStrLn $ "resolveEntryPoint " ++ pprid n ++ " " ++ pprid mb ++ " in\n" ++ show (Map.keys $ procedures env)
     return mb
 
 getModule :: Monad m => DafnyM m (Maybe Identifier)
