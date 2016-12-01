c083a058cc14218ad63d8f3308139592b1d71231
Hugo Pacheco <hpacheco@gmail.com>
2016-12-01 16:30:09 +0000

    dafny

--- src/Language/SecreC/Transformation/Dafny.hs
+++ src/Language/SecreC/Transformation/Dafny.hs
@@ -859,8 +859,7 @@ supersedesAssumption :: AnnDoc -> AnnDoc -> Bool
 supersedesAssumption (annK1,isFree1,vs1,e1,isLeak1) (annK2,isFree2,vs2,e2,isLeak2) =
     e1 == e2 && supersedesAnnKind annK1 annK2
 supersedesAnnKind :: AnnKind -> AnnKind -> Bool
-supersedesAnnKind InvariantK RequireK = True
-supersedesAnnKind InvariantK StmtK = True
+supersedesAnnKind _ InvariantK = False
 supersedesAnnKind x y = x == y
 
 addAssumptions :: DafnyK m => DafnyM m AnnsDoc -> DafnyM m AnnsDoc
